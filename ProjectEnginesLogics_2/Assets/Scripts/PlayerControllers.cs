2c699624cc3ee244131cb33d46c36791826ad2d3
Amanda <amandaduarte2012@gmail.com>
2019-12-03 18:57:42 -0500

    Created PlayerControllers_script

---
+++ ProjectEnginesLogics_2/Assets/Scripts/PlayerControllers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllers : MonoBehaviour   // Mouse clicks Left/Right
+{
+    TileLocation tileLoc;
+    int tileLocationX;
+    int tileLocationZ;
+
+    private void Start()
+    {
+        tileLoc = FindObjectOfType<TileLocation>();
+    }
+
+    private void Update()
+    {
+        RightClick();
+        LeftClick();
+    }
+
+    public void RightClick()   // ONLY MARKING / No revealing the tiles
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            tileLocationX = tileLoc.locationX;
+            tileLocationZ = tileLoc.locationZ;
+
+            Debug.Log("after RIGHT click: " + tileLocationX + ", " + tileLocationZ);
+
+            //foreach(TileSO tile in tile)
+        }
+    }
+
+    public void LeftClick()   // NO MARKING / just revealing tiles
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+
+}
