239471ec3aec7e8cf81a738cc490b08b3a2990e6
Amanda <amandaduarte2012@gmail.com>
2019-12-03 22:18:02 -0500

    Implemented the checking coordinates to Right Mouse click

--- ProjectEnginesLogics_2/Assets/Scripts/PlayerControllers.cs
+++ ProjectEnginesLogics_2/Assets/Scripts/PlayerControllers.cs
@@ -8,9 +8,12 @@ public class PlayerControllers : MonoBehaviour   // Mouse clicks Left/Right
     int tileLocationX;
     int tileLocationZ;
 
+    GridManager gridMG;
+
     private void Start()
     {
         tileLoc = FindObjectOfType<TileLocation>();
+        gridMG = FindObjectOfType<GridManager>();
     }
 
     private void Update()
@@ -21,22 +24,37 @@ public class PlayerControllers : MonoBehaviour   // Mouse clicks Left/Right
 
     public void RightClick()   // ONLY MARKING / No revealing the tiles
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
-            //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+            /*Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             tileLocationX = tileLoc.locationX;
             tileLocationZ = tileLoc.locationZ;
 
-            Debug.Log("after RIGHT click: " + tileLocationX + ", " + tileLocationZ);
+            Debug.Log("RIGHT CLICK: " + tileLocationX + ", " + tileLocationZ);
 
-            //foreach(TileSO tile in tile)
+            foreach (TileSO tile in gridMG.safeTileSOs)
+            {
+                
+            }*/
+
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000) && (!(hit.rigidbody == null)
+                || !(hit.collider == null)))
+            {
+                if (hit.collider.gameObject.tag == "Tile")
+                {
+                    TileLocation tileLoc = hit.collider.gameObject.GetComponent<TileLocation>();
+                    // in case there is a problem, try using GetComponentInParent<TileLocation>
+                    print(tileLoc.locationX.ToString() + ", " + tileLoc.locationZ.ToString());
+                }
+
+            }
         }
     }
 
     public void LeftClick()   // NO MARKING / just revealing tiles
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
         {
 
         }
